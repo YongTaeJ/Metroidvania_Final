83212b72428d0b4ddbffa2491205d157e04bb23a
dotoolboy <dotoolboy@naver.com>
2024-01-12 21:03:34 +0900

    [💡Feature] PlayerStateMachine
    - PlayerController
    - Player PPU 수정
    - Player animation
    - Player State Machine
    
    TODO - Input direction받는 방법 변경 해야됨

--- Assets/@Scripts/Player/Player.cs
+++ Assets/@Scripts/Player/Player.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody Rigidbody { get; private set; }
-    public Collider2D Collider { get; private set; }
-
+   
     private void Awake()
     {
-        Rigidbody = GetComponent<Rigidbody>();
-        Collider = GetComponent<Collider2D>();
+       
     }
 }
---
+++ Assets/@Scripts/Player/State/PlayerIdleState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerBaseState
+{
+    public PlayerIdleState(PlayerStateMachine machine) : base(machine)
+    {
+    }
+
+    
+}
