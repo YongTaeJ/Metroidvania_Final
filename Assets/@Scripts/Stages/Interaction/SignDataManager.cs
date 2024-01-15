751919e2523df91da45c417fa61ef5a68cbc8ced
Lawrence1031h <144416099+Lawrence1031@users.noreply.github.com>
2024-01-15 17:55:36 +0900

    [💡Feature] SignDataManager, Signs
    SignDataManager 구현
    Signs(표지판) 기능 구현
    화면에 overlay되는 타입,
    해당 오브젝트 위에 뜨는 타입 구현

---
+++ Assets/@Scripts/Stages/Interaction/SignDataManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignDataManager : MonoBehaviour
+{
+    public static SignDataManager Instance;
+
+    public Dictionary<string, string> SignMessages = new Dictionary<string, string>();
+
+    [SerializeField]
+    private InteractText _interactText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        SignMessages.Add("Sign1", _interactText.FirstSign);
+        SignMessages.Add("Sign2", _interactText.SecondSign);
+        SignMessages.Add("Sign3", _interactText.ThirdSign);
+        SignMessages.Add("Sign4", _interactText.FourthSign);
+    }
+
+    public string GetMessage(string SignId)
+    {
+        if (SignMessages.TryGetValue(SignId, out string message))
+        {
+            return message;
+        }
+        else
+        {
+            return "no message";
+        }
+    }
+}
