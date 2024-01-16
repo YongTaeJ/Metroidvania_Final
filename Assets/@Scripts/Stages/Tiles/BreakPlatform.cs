e9f3b920ecb94dffe3a3c4e0057cf36cd8f7fdbb
Lawrence1031h <144416099+Lawrence1031@users.noreply.github.com>
2024-01-16 17:56:06 +0900

    [➕Add] Chest, Platform
    Chest, BreakPlatform, DisapearPlatform Script 추가
    Test_JSM_01 Scene 추가

---
+++ Assets/@Scripts/Stages/Tiles/BreakPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private int _standCount = 1;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            if (collision.transform.position.y > this.transform.position.y)
+            {
+                _standCount--;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Rigidbody2D playerRigidbody = collision.collider.GetComponent<Rigidbody2D>();
+
+        if (playerRigidbody.velocity.y > 0 && _standCount < 1)
+        {
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/@Scripts/Stages/Tiles/TeleportTrap.cs
+++ Assets/@Scripts/Stages/Tiles/TeleportTrap.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TeleportTrap : MonoBehaviour
@@ -7,4 +9,38 @@ public class TeleportTrap : MonoBehaviour
     // damage 주는 함정
     // 플레이어의 체력을 받아서 일정 수치를 깍아서 반환
     // 플레이어를 특정 위치로 순간이동 시킴
+
+    [SerializeField]
+    private GameObject _panel;
+    [SerializeField]
+    private TextMeshProUGUI _mimicText;
+    [SerializeField]
+    private Vector3 _teleportPosition;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = _teleportPosition;
+
+            if (this.CompareTag("Mimic"))
+            {
+                MimicText();
+            }
+        }
+    }
+
+    private void MimicText()
+    {
+        _mimicText.text = "You just Actived Trap Card";
+        _panel.SetActive(true);
+
+        StartCoroutine(DeactiveText(2));
+    }
+
+    private IEnumerator DeactiveText(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        _panel.SetActive(false);
+    }
 }
