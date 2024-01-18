16d0f2f09a697b02dbd0512a31b6017e19c9d6a1
dotoolboy <dotoolboy@naver.com>
2024-01-18 20:37:47 +0900

    [🛠️bug Fix] Player Animation
    - 플레이어 애니메이션 수정중

--- Assets/@Scripts/Managers/AnimatorHash.cs
+++ Assets/@Scripts/Managers/AnimatorHash.cs
@@ -12,4 +12,12 @@ public static class AnimatorHash
     public static readonly int Idle = Animator.StringToHash("Idle");
     public static readonly int Dead = Animator.StringToHash("Dead");
     public static readonly int Hurt = Animator.StringToHash("Hurt");
+    public static readonly int Jump = Animator.StringToHash("Jump");
+    public static readonly int yVelocity = Animator.StringToHash("yVelocity");
+    public static readonly int WallSliding = Animator.StringToHash("WallSliding");
+    public static readonly int WallJump = Animator.StringToHash("WallJump");
+    public static readonly int Dash = Animator.StringToHash("Dash");
+    public static readonly int IsGrounded = Animator.StringToHash("Ground");
+    public static readonly int IsWall = Animator.StringToHash("Wall");
+    public static readonly int IsCeiling = Animator.StringToHash("Ceiling");
 }
