678e7392469e61bb48381fbaf68a048fd341e973
Lawrence1031h <144416099+Lawrence1031@users.noreply.github.com>
2024-01-30 20:57:59 +0900

    [💡Feature] Chest
    상자 관련 기능 추가
    (기존의 기능과 다르게 변경
    - 하나의 Chest가 아니라 추상 클래스로 Chest를 작성하고 하위에 ChestItem, ChestAbility, ChestMap 등을 작성해서 나눠서 만들 예정입니다)

---
+++ Assets/@Scripts/Stages/Interactive/Object/ChestItem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestItem : ChestBase
+{
+
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+    }
+
+    protected override void OnTriggerExit2D(Collider2D collision)
+    {
+        base.OnTriggerExit2D(collision);
+    }
+}
