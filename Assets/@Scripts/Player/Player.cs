1d2eb69e6675b90153802c461a6ad9b8b87bab1a
dotoolboy <dotoolboy@naver.com>
2024-01-31 20:24:49 +0900

    [💡Feature] Ui
    - 플레이어 스킬 UI
    - 시작 씬 틀만 만들어둠
    - Interaction 틀만
    - 플레이어 스크립트 정리

--- Assets/@Scripts/Player/Player.cs
+++ Assets/@Scripts/Player/Player.cs
@@ -2,15 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IDamagable
 {
     public int _maxHp = 10;
     public int _Hp;
     public int _damage = 5;
+    public bool IsHit
+    {
+        get
+        {
+            return _animator.GetBool(AnimatorHash.Hurt);
+        }
+        private set
+        {
+            _animator.SetBool(AnimatorHash.Hurt, value);
+        }
+    }
+    public bool Invincible
+    {
+        get => _invincible;
+        set
+        {
+            _invincible = value;
+            if (_invincible)
+            {
+                if (_coInvincible != null) StopCoroutine(_coInvincible);
+                _coInvincible = StartCoroutine(InvincibleTimer(_invincibilityTime));
+            }
+        }
+    }
+    private bool _invincible = false;
+    private Coroutine _coInvincible;
+    private float _invincibilityTime = 1f;
 
     public Animator _animator;
     public Rigidbody2D _rigidbody;
     public PlayerInputController _controller;
+
     private void Awake()
     {
         _controller = GetComponent<PlayerInputController>();
@@ -20,6 +48,64 @@ public class Player : MonoBehaviour
         GameManager.Instance.player = this;
     }
 
+    public void GetDamaged(int damage, Transform target)
+    {
+        if (Invincible == false)
+        {
+            Invincible = true;
+            StartCoroutine(FlashPlayer());
+            IsHit = true;
+            GameManager.Instance.player._Hp -= damage;
+            StartCoroutine(ResetHurtAnimation());
+            StartCoroutine(Knockback(target));
+
+            if (GameManager.Instance.player._Hp <= 0)
+            {
+                GameManager.Instance.player._Hp = 0;
+                _animator.SetBool(AnimatorHash.Dead, true);
+            }
+        }
+    }
+
+    private IEnumerator Knockback(Transform target)
+    {
+        float direction = Mathf.Sign(target.position.x - transform.position.x);
+        Vector2 knockbackDirection = new Vector2(-direction, 1f).normalized;
+        _rigidbody.velocity = knockbackDirection * 5f;
+        yield return new WaitForSeconds(0.2f);
+        _rigidbody.velocity = Vector2.zero;
+    }
+
+    private IEnumerator ResetHurtAnimation()
+    {
+        yield return new WaitForSeconds(0.3f);
+        IsHit = false;
+    }
+
+    private IEnumerator InvincibleTimer(float invincibilityTime)
+    {
+        yield return new WaitForSeconds(invincibilityTime);
+        Invincible = false;
+    }
+
+    // TODO 연속해서 맞을경우 알파값이 낮은 상태로 고정되는 버그 있음 애니메이션에 적용해서 이부분은 없앨수 있을듯
+    private IEnumerator FlashPlayer()
+    {
+        float flashSpeed = 0.1f;
+
+        while (Invincible)
+        {
+            Color originalColor = GetComponent<SpriteRenderer>().color;
+            Color flashColor = new Color(originalColor.r, originalColor.g, originalColor.b, 0.1f);
+            GetComponent<SpriteRenderer>().color = flashColor;
+
+            yield return new WaitForSeconds(flashSpeed);
+
+            GetComponent<SpriteRenderer>().color = originalColor;
+
+            yield return new WaitForSeconds(flashSpeed);
+        }
+    }
 
 
 
--- Assets/@Scripts/Player/PlayerInputController.cs
+++ Assets/@Scripts/Player/PlayerInputController.cs
@@ -5,25 +5,13 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInputController : MonoBehaviour, IDamagable
+public class PlayerInputController : MonoBehaviour
 {
     #region Properties
 
-    
-
-    #endregion
-
-    #region Fileds
-
-    private Animator _animator;
-    private Rigidbody2D _rigidbody;
-    private TouchingDirection _touchingDirection;
-    private bool _isFacingRight = true;
-    private float _speed = 10f;
-    private Vector2 _moveInput;
-
-    private bool _isWalking = false;
-    public bool Iswalking {get
+    public bool Iswalking
+    {
+        get
         {
             return _isWalking;
         }
@@ -34,6 +22,21 @@ public class PlayerInputController : MonoBehaviour, IDamagable
         }
     }
 
+    #endregion
+
+    #region Fileds
+
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+    private TouchingDirection _touchingDirection;
+    private Player _player;
+    private bool _isFacingRight = true;
+    private float _speed = 10f;
+    private Vector2 _moveInput;
+
+    private bool _isWalking = false;
+    
+
     private bool _isAttacking = false;
 
     //Jump
@@ -42,9 +45,9 @@ public class PlayerInputController : MonoBehaviour, IDamagable
     public int _jumpCount;
 
     //Gravity
-    public float _baseGravity = 2f;
-    public float _maxFallSpeed = 20f;
-    public float _fallSpeedMultiplier = 2f;
+    private float _baseGravity = 4f;
+    private float _maxFallSpeed = 20f;
+    private float _fallSpeedMultiplier = 2f;
 
     //Wall Slide
     private float _wallSlideSpeed = 0f;
@@ -77,35 +80,6 @@ public class PlayerInputController : MonoBehaviour, IDamagable
     public int _dashCount;
     private TrailRenderer _trailRenderer;
 
-    // 무적시간
-    public bool Invincible
-    {
-        get => _invincible;
-        set
-        {
-            _invincible = value;
-            if (_invincible)
-            {
-                if (_coInvincible != null) StopCoroutine(_coInvincible);
-                _coInvincible = StartCoroutine(InvincibleTimer(_invincibilityTime));
-            }
-        }
-    }
-
-    public bool IsHit { get
-        {
-            return _animator.GetBool(AnimatorHash.Hurt);
-        }
-        private set
-        {
-            _animator.SetBool(AnimatorHash.Hurt, value);
-        }
-    }
-
-    private bool _invincible = false;
-    private Coroutine _coInvincible;
-    private float _invincibilityTime = 1f;
-
     #endregion
 
     #region MonoBehaviour
@@ -116,13 +90,13 @@ public class PlayerInputController : MonoBehaviour, IDamagable
         _touchingDirection = GetComponent<TouchingDirection>();
         _trailRenderer = GetComponent<TrailRenderer>();
         _animator = GetComponent<Animator>();
-        
+        _player = GetComponent<Player>();
 
         //임시 스킬 초기화. ==================================================================
         _SwordAuror = GetComponent<Skill_SwordAuror>();
         _PlungeAttack = GetComponent<Skill_PlungeAttack>();
-        //_SwordAuror.Initialize();
-        //_PlungeAttack.Initialize();
+        _SwordAuror.Initialize();
+        _PlungeAttack.Initialize();
         //====================================================================================
     }
 
@@ -132,7 +106,7 @@ public class PlayerInputController : MonoBehaviour, IDamagable
         WallSlide();
         WallJump();
 
-        if (!_isWallJumping && !_isDashing && !IsHit)
+        if (!_isWallJumping && !_isDashing && !_player.IsHit)
         {
             _rigidbody.velocity = new Vector2(_moveInput.x * _speed, _rigidbody.velocity.y);
             Flip();
@@ -150,16 +124,55 @@ public class PlayerInputController : MonoBehaviour, IDamagable
 
     #region Movement
 
-    private void Gravity()
+    public void Move(InputAction.CallbackContext context)
     {
-        if (_rigidbody.velocity.y < 0)
+        _moveInput = context.ReadValue<Vector2>();
+
+        Iswalking = _moveInput != Vector2.zero;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (_jumpCount > 0)
         {
-            _rigidbody.gravityScale = _baseGravity * _fallSpeedMultiplier;
-            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, Mathf.Max(_rigidbody.velocity.y, -_maxFallSpeed));
+            if (context.started && _touchingDirection.IsGrounded)
+            {
+                if (!_isWallJumping)
+                {
+                    _animator.SetTrigger(AnimatorHash.Jump);
+                    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpPower);
+                    _jumpCount--;
+                }
+            }
+            else if (context.canceled)
+            {
+                if (!_isWallJumping)
+                {
+                    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+                    _jumpCount--;
+                }
+            }
         }
-        else
+
+        //Wall Jump
+        if (context.performed && _wallJumpTimer > 0f)
         {
-            _rigidbody.gravityScale = _baseGravity;
+            if (!_isWallJumping && _touchingDirection.IsWall)
+            {
+                _isWallJumping = true;
+                _rigidbody.velocity = new Vector2(_wallJumpDirection * _wallJumpPower.x, _wallJumpPower.y);
+                _wallJumpTimer = 0f;
+                _animator.SetTrigger(AnimatorHash.WallJump);
+                if (transform.localScale.x != _wallJumpDirection)
+                {
+                    _isFacingRight = !_isFacingRight;
+                    Vector3 Is = transform.localScale;
+                    Is.x *= -1f;
+                    transform.localScale = Is;
+                }
+
+                Invoke(nameof(CancelWallJump), _wallJumpTime + 0.1f);
+            }
         }
     }
 
@@ -197,13 +210,6 @@ public class PlayerInputController : MonoBehaviour, IDamagable
         _isWallJumping = false;
     }
 
-    public void Move(InputAction.CallbackContext context)
-    {
-        _moveInput = context.ReadValue<Vector2>();
-
-        Iswalking = _moveInput != Vector2.zero;
-    }
-
     public void Attack(InputAction.CallbackContext context)
     {
         if (context.performed && _isAttacking == false)
@@ -221,6 +227,36 @@ public class PlayerInputController : MonoBehaviour, IDamagable
         _isAttacking = false;
     }
 
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (context.performed && _canDash == true)
+        {
+            StartCoroutine(CoDash());
+        }
+    }
+
+    private IEnumerator CoDash()
+    {
+        if (_dashCount > 0)
+        {
+            _animator.SetTrigger(AnimatorHash.Dash);
+            _canDash = false;
+            _isDashing = true;
+            _player.Invincible = true;
+            float originalGravity = _baseGravity;
+            _rigidbody.gravityScale = 0f;
+            _rigidbody.velocity = new Vector2(transform.localScale.x * _dashPower, 0f);
+            _trailRenderer.emitting = true;
+            yield return new WaitForSeconds(_dashTime);
+            _trailRenderer.emitting = false;
+            _rigidbody.gravityScale = originalGravity;
+            _isDashing = false;
+            _dashCount--;
+            yield return new WaitForSeconds(0.1f);
+            _canDash = true;
+        }
+    }
+
     // 임시 ===================================================================
     private Skill_SwordAuror _SwordAuror;
     private Skill_PlungeAttack _PlungeAttack;
@@ -235,83 +271,33 @@ public class PlayerInputController : MonoBehaviour, IDamagable
         else if (Input.GetKey(KeyCode.DownArrow) && !_touchingDirection.IsGrounded && context.performed)
         {
             _PlungeAttack.Activate();
-            Invincible = true;
+            _player.Invincible = true;
         }
     }
 
-    public void Jump(InputAction.CallbackContext context)
+    public void Interaction(InputAction.CallbackContext context)
     {
-        if (_jumpCount > 0)
-        {
-            if (context.started && _touchingDirection.IsGrounded)
-            {
-                if(!_isWallJumping)
-                {
-                    _animator.SetTrigger(AnimatorHash.Jump);
-                    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpPower);
-                    _jumpCount--;
-                }
-            }
-            else if (context.canceled)
-            {
-                if (!_isWallJumping)
-                {
-                    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
-                    _jumpCount--;
-                }
-            }
-        }
-
-        //Wall Jump
-        if(context.performed && _wallJumpTimer > 0f)
-        {
-            if (!_isWallJumping && _touchingDirection.IsWall)
-            {
-                _isWallJumping = true;
-                _rigidbody.velocity = new Vector2(_wallJumpDirection * _wallJumpPower.x, _wallJumpPower.y);
-                _wallJumpTimer = 0f;
-                _animator.SetTrigger(AnimatorHash.WallJump);
-                if (transform.localScale.x != _wallJumpDirection)
-                {
-                    _isFacingRight = !_isFacingRight;
-                    Vector3 Is = transform.localScale;
-                    Is.x *= -1f;
-                    transform.localScale = Is;
-                }
-
-                Invoke(nameof(CancelWallJump), _wallJumpTime + 0.1f);
-            }
-        }
+        //if (상호작용이 가능한 오브젝트 && context.performed)
+        //{
+        //    상호작용 오브젝트에서 실행될 메서드?
+        //}
     }
 
-    public void Dash(InputAction.CallbackContext context)
+    private void Gravity()
     {
-        if (context.performed && _canDash == true)
+        if (_rigidbody.velocity.y < 0)
         {
-            StartCoroutine(CoDash());
+            _rigidbody.gravityScale = _baseGravity * _fallSpeedMultiplier;
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, Mathf.Max(_rigidbody.velocity.y, -_maxFallSpeed));
+        }
+        else
+        {
+            _rigidbody.gravityScale = _baseGravity;
         }
     }
 
-    private IEnumerator CoDash()
-    {
-        if(_dashCount > 0)
-        {
-            _animator.SetTrigger(AnimatorHash.Dash);
-            _canDash = false;
-            _isDashing = true;
-            float originalGravity = _baseGravity;
-            _rigidbody.gravityScale = 0f;
-            _rigidbody.velocity = new Vector2(transform.localScale.x * _dashPower, 0f);
-            _trailRenderer.emitting = true;
-            yield return new WaitForSeconds(_dashTime);
-            _trailRenderer.emitting = false;
-            _rigidbody.gravityScale = originalGravity;
-            _isDashing = false;
-            _dashCount--;
-            yield return new WaitForSeconds(0.1f);
-            _canDash = true;
-        }
-    }
+    
+    
 
     #endregion
 
@@ -345,64 +331,7 @@ public class PlayerInputController : MonoBehaviour, IDamagable
     #region Attribute Method
 
     // Controller에 필요없음
-    public void GetDamaged(int damage, Transform target)
-    {
-        if (Invincible == false)
-        {
-            Invincible = true;
-            StartCoroutine(FlashPlayer());
-            IsHit = true;
-            GameManager.Instance.player._Hp -= damage;
-            StartCoroutine(ResetHurtAnimation());
-            StartCoroutine(Knockback(target));
-
-            if (GameManager.Instance.player._Hp <= 0)
-            {
-                GameManager.Instance.player._Hp = 0;
-                _animator.SetBool(AnimatorHash.Dead, true);
-            }
-        }
-    }
-
-    private IEnumerator Knockback(Transform target)
-    {
-        float direction = Mathf.Sign(target.position.x - transform.position.x);
-        Vector2 knockbackDirection = new Vector2(-direction, 1f).normalized;
-        _rigidbody.velocity = knockbackDirection * 5f;
-        yield return new WaitForSeconds(0.2f);
-        _rigidbody.velocity = Vector2.zero;
-    }
-
-    private IEnumerator ResetHurtAnimation()
-    {
-        yield return new WaitForSeconds(0.3f);
-        IsHit = false;
-    }
-
-    private IEnumerator InvincibleTimer(float invincibilityTime)
-    {
-        yield return new WaitForSeconds(invincibilityTime);
-        Invincible = false;
-    }
-
-    // TODO 연속해서 맞을경우 알파값이 낮은 상태로 고정되는 버그 있음 애니메이션에 적용해서 이부분은 없앨수 있을듯
-    private IEnumerator FlashPlayer()
-    {
-        float flashSpeed = 0.1f;
-
-        while (Invincible)
-        {
-            Color originalColor = GetComponent<SpriteRenderer>().color;
-            Color flashColor = new Color(originalColor.r, originalColor.g, originalColor.b, 0.1f);
-            GetComponent<SpriteRenderer>().color = flashColor;
-
-            yield return new WaitForSeconds(flashSpeed);
-
-            GetComponent<SpriteRenderer>().color = originalColor;
-
-            yield return new WaitForSeconds(flashSpeed);
-        }
-    }
+   
 
     #endregion
 }
