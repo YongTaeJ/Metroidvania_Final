785505db58a98819759c45852a1d5942caf00d09
Lawrence1031h <144416099+Lawrence1031@users.noreply.github.com>
2024-01-31 16:25:31 +0900

    [🛠️Fix] Chest Script Fix
    상자 스크립트 수정

--- Assets/@Scripts/Stages/Interactive/Object/ChestItem.cs
+++ Assets/@Scripts/Stages/Interactive/Object/ChestItem.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ChestItem : ChestBase
 {
+    // 골드나 아이템은 같은 방식으로 작동되게 할 것이므로 같은 ChestItem를 사용
+
+    [SerializeField] private int _chestGold;
+    [SerializeField] private ItemType _chestItem;
+    [SerializeField] private int _chestItemID;
 
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         base.OnTriggerEnter2D(collision);
+
+        if (_chestItem == ItemType.Gold)
+        {
+            // 마지막 150 부분에 골드량을 조절할 수 있게 변경하면 될듯
+            ItemManager.Instance.AddItem(_chestItem, _chestItemID, 150);
+        }
+        else
+        {
+            ItemManager.Instance.AddItem(_chestItem, _chestItemID);
+        }
     }
 
     protected override void OnTriggerExit2D(Collider2D collision)
     {
         base.OnTriggerExit2D(collision);
+    }
+
+    protected override void ChestText()
+    {
+        ItemManager.Instance.GetItemData(_chestItem, _chestItemID);
+        _chestText.text = "You got " + _chestItem;
+        _panel.SetActive(true);
     }
 }
--- Assets/@Scripts/Stages/Interactive/Object/ChestMap.cs
+++ Assets/@Scripts/Stages/Interactive/Object/ChestMap.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class ChestMap : ChestBase
 {
-    [SerializeField] private MapData _mapdata;
+    [SerializeField] private MapData _mapData;
+    [SerializeField] private int _mapDataLimit;
+
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         base.OnTriggerEnter2D(collision);
-        _mapdata._curMapData++;
+
+        if (_mapData._curMapData > _mapDataLimit)
+        {
+            _chestText.text = "You Already have this map data";
+        }
+        else
+        {
+            _chestText.text = "You acquired map data";
+            _mapData._curMapData++;
+        }
     }
 
     protected override void OnTriggerExit2D(Collider2D collision)
     {
         base.OnTriggerExit2D(collision);
     }
-
 }
