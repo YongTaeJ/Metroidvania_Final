59bb628fdc3c81effe9b9d0a7f7509ed0114eec2
Lawrence1031h <144416099+Lawrence1031@users.noreply.github.com>
2024-02-02 17:35:50 +0900

    [🛠️Fix] InteractUI delete, portal interact
    InteractUI 제거 -> UIManager의 기능을 바로 사용하도록 변경
    
    portal의 interact 시스템 변경
    M키를 눌렀을 때 포탈 근처면 사용 가능에서
    포탈 근처에서 상호 작용시 포탈 사용 가능으로 변경

--- Assets/@Scripts/Stages/WorldMap/PortalTrigger.cs
+++ Assets/@Scripts/Stages/WorldMap/PortalTrigger.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PortalTrigger : MonoBehaviour
 {
     public bool CanUsePortal = false;
+    protected PlayerInput _playerInput;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            _playerInput = collision.GetComponent<PlayerInput>();
+            var playerInputController = collision.GetComponent<PlayerInputController>();
+            playerInputController.OnInteraction += UsePortal;
+            UIManager.Instance.OpenPopupUI(PopupType.Interact);
+
             MapManager.Instance._portalTrigger = this;
             CanUsePortal = true;
         }
@@ -19,8 +26,18 @@ public class PortalTrigger : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            var playerInputController = collision.GetComponent<PlayerInputController>();
+            playerInputController.OnInteraction -= UsePortal;
+            UIManager.Instance.ClosePopupUI(PopupType.Interact);
+
             MapManager.Instance._portalTrigger = null;
             CanUsePortal = false;
         }
+    }
+
+    private void UsePortal()
+    {
+        MapManager.Instance.OpenLargeMap();
+        UIManager.Instance.ClosePopupUI(PopupType.Interact);
     }
 }
