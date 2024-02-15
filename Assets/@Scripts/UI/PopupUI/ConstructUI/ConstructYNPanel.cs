8cc919cf7b47a9b76733efa5e729fddf307e8aa3
Lawrence1031h <144416099+Lawrence1031@users.noreply.github.com>
2024-02-15 20:20:54 +0900

    [💾Save] save
    save for merge

--- Assets/@Scripts/UI/PopupUI/ConstructUI/ConstructYNPanel.cs
+++ Assets/@Scripts/UI/PopupUI/ConstructUI/ConstructYNPanel.cs
@@ -1,6 +1,8 @@
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ConstructYNPanel : YNPanel
@@ -8,12 +10,14 @@ public class ConstructYNPanel : YNPanel
     #region variables
     private int _ID;
     private Action OnRefresh;
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
     #endregion
-    
+
     public void Initialize(int ID)
     {
         _ID = ID;
         base.Initialize();
+        cinemachineVirtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
     }
 
     public void InitAction(ConstructUI constructUI)
@@ -42,8 +46,59 @@ public class ConstructYNPanel : YNPanel
 
         ItemManager.Instance.AddItem(ItemType.Building, _ID);
 
+        BuildingConstructAnimation(_ID);
+
         OnRefresh.Invoke();
-        
+
         Destroy(gameObject);
+    }
+
+    private void BuildingConstructAnimation(int buildingID)
+    {
+        var buildingSO = SOManager.Instance.GetBuildingSO(buildingID);
+        Transform originalTarget = cinemachineVirtualCamera.Follow;
+
+        GameObject buildingPrefab = buildingSO._buildingData.buildingPrefab;
+        Vector3 buildingPosition = buildingSO._buildingData.buildingPosition;
+        GameObject buildingObject = Instantiate(buildingPrefab, buildingPosition, Quaternion.identity);
+
+        HideUI(true);
+        ChangeCinemachineCameraTarget(buildingPosition);
+
+        StartCoroutine(WaitForAnimation(buildingObject));
+
+        cinemachineVirtualCamera.Follow = originalTarget;
+    }
+    public void HideUI(bool hide)
+    {
+        Canvas[] canvases = FindObjectsOfType<Canvas>();
+        foreach (Canvas canvas in canvases)
+        {
+            canvas.enabled = !hide;
+        }
+    }
+
+    private void ChangeCinemachineCameraTarget(Vector3 Position)
+    {
+        Transform buildingTransform = new GameObject("BuildingTarget").transform;
+        buildingTransform.position = Position;
+        cinemachineVirtualCamera.Follow = buildingTransform;
+    }
+
+    IEnumerator WaitForAnimation(GameObject buildingPrefab, float waitTime = 0f)
+    {
+        Animator animator = buildingPrefab.GetComponent<Animator>();
+
+        if (animator != null)
+        {
+            AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+            waitTime = clipInfo[0].clip.length;
+            Debug.Log(waitTime);
+        }
+
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Return");
+
+        HideUI(false);
     }
 }
