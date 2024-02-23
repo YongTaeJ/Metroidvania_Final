5ccd896499cab8ba637d642c15377910ef269cd1
Lawrence1031h <144416099+Lawrence1031@users.noreply.github.com>
2024-02-23 12:20:18 +0900

    [🛠️Fix] SignManager Singleton
    사인 매니저 싱글톤화 및 표지판 추가

--- Assets/@Scripts/Stages/Interactive/Object/SignDataManager.cs
+++ Assets/@Scripts/Stages/Interactive/Object/SignDataManager.cs
@@ -7,8 +7,7 @@ public class SignDataManager : Singleton<SignDataManager>
 {
     public Dictionary<string, string> SignMessages = new Dictionary<string, string>();
 
-    [SerializeField]
-    private SignTexts _signTexts;
+    private SignTexts _signTexts = new SignTexts();
 
     protected override void Awake()
     {
@@ -19,6 +18,8 @@ public class SignDataManager : Singleton<SignDataManager>
         SignMessages.Add("Sign02", _signTexts.Sign02);
         SignMessages.Add("Sign03", _signTexts.Sign03);
         SignMessages.Add("Sign04", _signTexts.Sign04);
+        SignMessages.Add("Sign05", _signTexts.Sign05);
+        SignMessages.Add("Sign06", _signTexts.Sign06);
     }
 
     public string GetMessage(string SignId)
