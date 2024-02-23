18e714f35ff2f50bc367d2ce201181e04a1293ff
dotoolboy <dotoolboy@naver.com>
2024-02-23 18:52:11 +0900

    [🛠️Fix] Loading
    - 디자인 수정

--- Assets/@Scripts/UI/LoadingImage.cs
+++ Assets/@Scripts/UI/LoadingImage.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LoadingImage : MonoBehaviour
 {
-    public Text loadingText; // Unity 인스펙터에서 할당
+    public TMP_Text loadingText; // Unity 인스펙터에서 할당
 
     private void Start()
     {
@@ -19,14 +20,14 @@ public class LoadingImage : MonoBehaviour
     {
         while (true) // 무한 루프로 반복
         {
-            loadingText.text = "Now Loading";
-            yield return new WaitForSeconds(0.5f); // 0.5초 기다림
-            loadingText.text = "Now Loading.";
-            yield return new WaitForSeconds(0.5f); // 0.5초 기다림
-            loadingText.text = "Now Loading..";
-            yield return new WaitForSeconds(0.5f); // 0.5초 기다림
-            loadingText.text = "Now Loading...";
-            yield return new WaitForSeconds(0.5f); // 0.5초 기다림
+            loadingText.text = "굴러가는중.";
+            yield return new WaitForSeconds(0.1f); // 0.5초 기다림
+            loadingText.text = "굴러가는중..";
+            yield return new WaitForSeconds(0.1f); // 0.5초 기다림
+            loadingText.text = "굴러가는중...";
+            yield return new WaitForSeconds(0.1f); // 0.5초 기다림
+            loadingText.text = "굴러가는중....";
+            yield return new WaitForSeconds(0.1f); // 0.5초 기다림
         }
     }
 }
