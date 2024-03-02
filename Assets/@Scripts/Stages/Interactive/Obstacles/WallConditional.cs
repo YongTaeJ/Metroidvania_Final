87cf08a8be373be19ab5de16cc58289451cde854
dotoolboy <dotoolboy@naver.com>
2024-03-02 21:49:39 +0900

    [🛠️Fix] Map Collider
    - 맵 콜라이더 수정, 몬스터 스폰도 합쳐진 부분들 수정

--- Assets/@Scripts/Stages/Interactive/Obstacles/WallConditional.cs
+++ Assets/@Scripts/Stages/Interactive/Obstacles/WallConditional.cs
@@ -9,11 +9,6 @@ public class WallConditional : WallBase
 
     protected override void WallReact(Collider2D collision)
     {
-        if (collision.CompareTag("PlayerAttack"))
-        {
-            SFX.Instance.PlayOneShot(ResourceManager.Instance.GetAudioClip("WallReact"));
-        }
-
         if (collision.CompareTag("Player"))
         {
             if (ItemManager.Instance.HasItem(ItemType.Equipment, _itemID))
